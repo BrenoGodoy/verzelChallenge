390438463ffbdf5bf6cc385229b95f7a69709245
Breno Godoy <godoyy25z@gmail.com>
2023-01-24 16:55:44 -0300

    fix: Adiciona nome correto ao placeholder do input de Código de acesso

--- front-end/src/Pages/Register.jsx
+++ front-end/src/Pages/Register.jsx
@@ -90,7 +90,7 @@ function Register() {
               </div>
               <div className="my-3">
                 <label htmlFor="registro" className="block mb-2 text-sm font-medium text-gray-900">Código de Acesso de registro (simulação de código de segurança gerado pela empresa para registrar um administrador)</label>
-                <input type="text" name="registro" id="registro" className="bg-gray-100 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 hover:bg-gray-50" placeholder="Sua senha"  
+                <input type="text" name="registro" id="registro" className="bg-gray-100 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 hover:bg-gray-50" placeholder="Código"  
                 />
               </div>
               <button disabled={ isDisable } onClick={(e) => handleClick(e.target)} type="button" className="my-10 w-full text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:opacity-50">Registrar</button>
